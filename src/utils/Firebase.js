39ef84e664f531ec38889de4e9233667bfee18ae
Ibrahim Kolabalogun <32678498+Kolabalogun@users.noreply.github.com>
2023-08-01 20:13:21 +0100

    ENV

--- src/utils/Firebase.js
+++ src/utils/Firebase.js
@@ -4,18 +4,19 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA8bjKmLo7ADtWc8zcvEIMWpBXnV038aM0",
-  authDomain: "aisha-bankole.firebaseapp.com",
-  projectId: "aisha-bankole",
-  storageBucket: "aisha-bankole.appspot.com",
-  messagingSenderId: "964549924786",
-  appId: "1:964549924786:web:b27094f7d788417a8436ab",
-  // apiKey: process.env.FIREBASE_API_KEY,
-  // authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  // projectId: process.env.FIREBASE_PROJECT_ID,
-  // storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  // messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  // appId: process.env.FIREBASE_APP_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+
+  // apiKey: "AIzaSyA8bjKmLo7ADtWc8zcvEIMWpBXnV038aM0",
+  // authDomain: "aisha-bankole.firebaseapp.com",
+  // projectId: "aisha-bankole",
+  // storageBucket: "aisha-bankole.appspot.com",
+  // messagingSenderId: "964549924786",
+  // appId: "1:964549924786:web:b27094f7d788417a8436ab",
 };
 
 const app = initializeApp(firebaseConfig);
