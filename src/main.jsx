11ac4cad04fc264c0e02b2dd69de4416afa9cfa8
Ibrahim Kolabalogun <32678498+Kolabalogun@users.noreply.github.com>
2023-10-17 22:40:43 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -12,19 +12,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </Provider>
   </React.StrictMode>
 );
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js").then(
-      (registration) => {
-        console.log(
-          "ServiceWorker registration successful with scope: ",
-          registration.scope
-        );
-      },
-      (err) => {
-        console.log("ServiceWorker registration failed: ", err);
-      }
-    );
-  });
-}
