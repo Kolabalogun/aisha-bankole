2247212d86e6a128d5c6d61626c4b7f133e926d2
Ibrahim Kolabalogun <32678498+Kolabalogun@users.noreply.github.com>
2024-09-11 11:22:40 +0100

    feat: loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,6 +1,6 @@
 const Loader = () => {
   return (
-    <div className="h-screen flex items-center justify-center bg-[#996AA0]">
+    <div className="h-screen flex items-center justify-center bg-[#000]">
       <span className="loader"></span>
     </div>
   );
